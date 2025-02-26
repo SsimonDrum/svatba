4cda326af35be5621797d74ac527eecd2e98832b
SsimonDrum <ssimondrum@gmail.com>
2025-02-26 14:05:36 +0100

    * make structure flat

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ function openModal() {
     let modal = document.getElementById('mapModal');
     let modalImg = document.getElementById('mapImage');
     modal.style.display = 'flex';
-    modalImg.src = 'assets/images/mapa.png';
+    modalImg.src = 'mapa.png';
 }
 
 // Close the modal
