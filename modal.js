eeed86a5ec0f95692591110a3bb17ec3de557b25
SsimonDrum <50425813+SsimonDrum@users.noreply.github.com>
2025-02-26 13:25:02 +0100

    #1 pridej welcome page a proklik na info (#2)

---
+++ modal.js
@@ -0,0 +1,12 @@
+function openModal() {
+    let modal = document.getElementById('mapModal');
+    let modalImg = document.getElementById('mapImage');
+    modal.style.display = 'flex';
+    modalImg.src = '/assets/images/mapa.png';
+}
+
+// Close the modal
+function closeModal() {
+    let modal = document.getElementById('mapModal');
+    modal.style.display = 'none';
+}
