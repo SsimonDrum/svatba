59cdf076fdc6c48276f2a555bb76174bc77f2ef9
SsimonDrum <50425813+SsimonDrum@users.noreply.github.com>
2025-03-04 16:43:59 +0100

    #7 create better layout

--- modal.js
+++ modal.js
@@ -1,11 +1,8 @@
 function openModal() {
     let modal = document.getElementById('mapModal');
-    let modalImg = document.getElementById('mapImage');
     modal.style.display = 'flex';
-    modalImg.src = 'https://ssimondrum.github.io/svatba/mapa_arealu.png';
 }
 
-// Close the modal
 function closeModal() {
     let modal = document.getElementById('mapModal');
     modal.style.display = 'none';
