5d9b0bec6a0353b1ecce1a17916e36bbed314679
SsimonDrum <ssimondrum@gmail.com>
2025-03-30 13:48:30 +0200

    * #10 Remove map update doodles

--- modal.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-    let modal = document.getElementById('mapModal');
-    modal.style.display = 'flex';
-}
-
-function closeModal() {
-    let modal = document.getElementById('mapModal');
-    modal.style.display = 'none';
-}
